e0ba96dc4d301ce7ee3935a32dbcb2b7a2779e02
gmoccio <gmoccio812@gmail.com>
2026-02-27 00:13:48 -0500

    It now accepts pemdas, however does not print the complete total, so if given 3 + 4 * 2, returns 7, 8

--- calculator.py
+++ calculator.py
@@ -16,48 +16,43 @@ def calcbrains(): #the "brains" of the calculator where the user input is parsed
         else:
             return equation #allows for equation to be accessed outside this function
 
-def storage(equation): #The numbers and operaters the user inputs are then parsed through here, with equation being fed into this function
-    equation = equation.split() #this takes user input and splits each part of it into a list, so 5 + 3 becomes [5, +, 3]
-    numbers = []
-    operators = []
-    for i in equation:
-        if i.isnumeric() or "." in i: #this if statment checks equation (which is now a list) to see which values are numerical and puts them in the newly created list numbers
-            numbers.append(i)
-        else:
-            operators.append(i) #this does the same for the mathematical operators
-    return numbers, operators #allows for these new lists to be available outside the function
 
-def numcheck(numbers): #a function to determine whether or not the inputted numbers should be an integer (non-decimal) or a float (decimal)
+def numcheck(equation): #a function to determine whether or not the inputted numbers should be an integer (non-decimal) or a float (decimal)
+    print("numparse reached")
+    placeholder = equation.split()
     numparse = []
-    for i in numbers: #if the number has a decimal, make it a float, or else make it an int and put it in numparse
+    for i in placeholder: #if the number has a decimal, make it a float, or else make it an int and put it in numparse
         if "." in i:
             numparse.append(float(i))
-        else:
+        elif i.isnumeric():
             numparse.append(int(i))
+        else:
+            numparse.append(i)
+    print(numparse)
+    print("numparse success")
     return numparse
     
 
-def math(numparse, operators): #the "brains" of the calculator, basically just the logic of the arithmetic. because functions are self-contained, the parameters are necessary for the function to read things outside it
-    runtot = numparse[0] 
-    for num, op in zip(numparse[1:], operators): 
-        if op == "+":                            
-            runtot = runtot + num
-        elif op == "-":
-            runtot = runtot - num
-        elif op == "*":
-            runtot = runtot * num
-        elif op == "/":
-            runtot = runtot / num
-    total = runtot
-    return total
+def math(numparse): #the "brains" of the calculator, basically just the logic of the arithmetic. because functions are self-contained, the parameters are necessary for the function to read things outside it 
+    print("math achieved")
+    for index, i in enumerate(numparse):
+        if i == "*":
+            numparse.append(numparse[index-1] * numparse[index+1])
+        if i == "/":
+            numparse.append(numparse[index-1] / numparse[index+1])
+        if i == "+":
+            numparse.append(numparse[index-1] + numparse[index+1])
+        if i == "-":
+            numparse.append(numparse[index-1] - numparse[index+1])
+    print("successful mathing")
+    return numparse
 
 # here is where the main starts
 
 memory = []
 while True:
     equation = calcbrains()
-    numbers, operators = storage(equation)
-    numparse = numcheck(numbers)
-    total = math(numparse, operators)
+    numparse = numcheck(equation)
+    total = math(numparse)
     print(total) #prints finalized answer in sentence format
     memory.append(total) #adds answer to calculator memory
