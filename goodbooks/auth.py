63699cd9983457c39ec88a26645b47ce7b2a83e7
gmoccio <gmoccio812@gmail.com>
2026-03-17 09:22:23 -0400

    added getpass, though may have to add something better for security since this seems to not work in terminal

--- goodbooks/auth.py
+++ goodbooks/auth.py
@@ -1,18 +1,19 @@
 import database
 import bcrypt
+import getpass
 
 def login():
     while True:
         uinput = input("Login or Register?: ")
         if uinput == "register":
             username = input("Create username: ")
-            password = input("Create password: " )
+            password = getpass.getpass("Create password: " )
             hashedpw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
             database.insert_user(username, hashedpw)
             print("Profile Created!")
         elif uinput == "login":
             username = input("Enter your username: ")
-            password = input("Enter password: ")
+            password = getpass.getpass("Enter password: ")
             user_id = database.login_user(username, password)
 
             if user_id:
