c99a9d247f6f32108c0f00d913dbd756fe3b3bb7
gmoccio <gmoccio812@gmail.com>
2026-03-17 10:06:41 -0400

    added reviews to main_menu

--- goodbooks/main.py
+++ goodbooks/main.py
@@ -1,6 +1,7 @@
 import database
 import auth
 import books
+
 def main():
     database.create_tables()
     user_id = auth.login()
@@ -12,12 +13,17 @@ def main_menu(user_id):
         print("What would you like to do?: ")
         print("1. Search books")
         print("2. View reading list")
-        print("3. quit")
+        print("3. Review read books")
+        print("4. quit")
         uinput = input("Enter choice: ")
         if uinput == "1":
             books.search_menu(user_id)
         elif uinput == "2":
             database.view_readlist(user_id)
+        elif uinput == "3":
+            books.leave_review(user_id)
+        elif uinput == "4":
+            break
 
 if __name__ == "__main__":
     main()
